e2be695fdb08fb02e3243ad07591b184e1163f58
faddey <mohamedavdoll@gmail.com>
2022-03-13 20:18:59 +0300

    write all combinations

--- true_2348/main.cpp
+++ true_2348/main.cpp
@@ -17,5 +17,10 @@ int main()
 	    	cin >> cur_num;
 	    	c[cur_num%4] ++;
 		}
+    cout << (c[0]*(c[0]-1)*(c[0]-2))/6<< endl;
+    cout << c[0]*c[1]*c[3]<< endl;
+    cout << c[0]*c[2]*(c[2]-1)*0.5<< endl;
+    cout << c[2]*c[1]*(c[1]-1)*0.5<< endl;
+    cout << c[2]*c[3]*(c[3]-1)*0.5<< endl;
     return 0;
 }
