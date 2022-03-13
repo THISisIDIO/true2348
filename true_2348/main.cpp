1cc848d3c8aac4e03840cf0ac9230dba13c7472e
faddey <mohamedavdoll@gmail.com>
2022-03-13 19:51:59 +0300

    write sum mod 4 = 0

--- true_2348/main.cpp
+++ true_2348/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 /* 1. + запомнить и вывести числа
    2. + вывести тройки чисел
-   3. вывести сумму троек
+   3. + вывести сумму троек
    4. вывести сумму делящююся на 4
    5. вывести их кол-во
 */
@@ -17,6 +17,10 @@ int main()
     for(int i(0);i<n; i++)
     for(int j(i+1);j<n; j++)
     for(int l(j+1);l<n; l++)
-        cout << numbers[i]+numbers[j]+numbers[l]<< endl;
+        {
+            int sum1=numbers[i]+numbers[j]+numbers[l];
+            if (sum1%4==0)
+                cout << sum1<< endl;
+        }
     return 0;
 }
