51bf197af43a46de8d5fdaa9eb454be7f3cbbb12
faddey <mohamedavdoll@gmail.com>
2022-03-13 19:48:52 +0300

    write sum triples

--- true_2348/main.cpp
+++ true_2348/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 /* 1. + запомнить и вывести числа
-   2. вывести тройки чисел
+   2. + вывести тройки чисел
    3. вывести сумму троек
    4. вывести сумму делящююся на 4
    5. вывести их кол-во
@@ -17,6 +17,6 @@ int main()
     for(int i(0);i<n; i++)
     for(int j(i+1);j<n; j++)
     for(int l(j+1);l<n; l++)
-        cout << numbers[i]<<" "<<numbers[j]<<" "<<numbers[l]<< endl;
+        cout << numbers[i]+numbers[j]+numbers[l]<< endl;
     return 0;
 }
