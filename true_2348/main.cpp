5bbeef772737f51d3118cc4e487bd0be36bc0757
faddey <mohamedavdoll@gmail.com>
2022-03-13 20:09:04 +0300

    read numbers

--- true_2348/main.cpp
+++ true_2348/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
 using namespace std;
-/* 1. ввести числа
+/* 1. + ввести числа
    2. вы€снить кол-во чисел дел€щихс€ на 4 с остатком(0,1,2,3)
    3. вычислить комбинации(000,013,022,112,233)
    4. сложить комбинации
 */
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n(0);
+    int cur_num(0);
+    cin >> n;
+    for (int i(0); i<n;i++)
+        cin>> cur_num;
     return 0;
 }
